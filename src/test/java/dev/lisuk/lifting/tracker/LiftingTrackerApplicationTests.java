43ac1a0341b7f17ace73b3f3d36add814fc7ef93
David Lisuk <david@lisuk.dev>
2023-03-27 11:17:20 -0400

    Initial commit from springboot

---
+++ src/test/java/dev/lisuk/lifting/tracker/LiftingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lisuk.lifting.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
